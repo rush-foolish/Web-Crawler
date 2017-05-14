69d3d8fba782559ff61550417c5b562d862f9762
刘瑞琴 <richearday@gmail.com>
2017-05-15 00:17:35 +0800

    add new script

--- stock_indstr_v2.py
+++ stock_indstr_v2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 #####################################################
-#	File Name: stock_indstr.py
+#	File Name: stock_indstr_v2.py
 #	Author: Rachel Liu
-#	Created Time: 2017-05-09
-#	Description: 
+#	Created Time: 2017-05-13
+#	Description: As the stock_industr need more than 30 min
+#   to run completely, do some improvement
 #	
 #	Modified History: 
 #	
@@ -13,11 +14,10 @@
 
 import urllib.request
 from bs4 import BeautifulSoup
-# from multiprocessing import Pool
-import time
+import datetime
 import pymysql
-import re
-# import http.client
+import time
+import http
 
 def cat_find(s):       
     global NUM,l
@@ -41,70 +41,64 @@ def load_db(table, values):
     cursor.executemany(sql, values)
 
 ## request webpage
-def url_findresult(url, tag, attr={}):
+def url_request(url):
     headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
     request = urllib.request.Request(url=url,headers=headers)
     try:      
         response = urllib.request.urlopen(request)
         result = response.read().decode('gbk')
     except:
+        print(url)
         time.sleep(10)
-        response = urllib.request.urlopen(request)
-        result = response.read().decode('gbk')
+        connection = http.client.HTTPConnection(url.lstrip("http://"))
+        connection.request('GET')
+        result = connection.getresponse().read().decode('gbk')
 
     soup = BeautifulSoup(result, "html.parser")
-    find_cont = soup.find(tag, attrs= attr)
-    return find_cont
+    return soup
 
 ## fetch stock details
-def stock_details(link, cat):  
-    lurl = 'http://quote.stockstar.com' + link   
+def stock_details(lurl, pagenum, cat):  
     # prefix of sub－category url
     if cat.startswith('M'): 
         suburl_p = lurl.replace('.shtml','') + '_1_0_%d.html'#order by stock_cd ascending 
     else:
         suburl_p = lurl.replace('.shtml','') + '_0_0_%d.html'
-    pgnm = 1 ##page start at 1
+
     stocks = []  
-    ## FETCH the data from each subclass          
-    while True:
-        suburl = suburl_p%pgnm
-        try:
-            content = url_findresult(suburl,'tbody', {"class": "tbody_right"}).contents
-        except AttributeError as e:
-            print(suburl)
-            print(e)
-
-        rownum = len(content) - 1
-        # check if current page is the last pagenum+1; in last pagenum+1, there is no stock in the list               
-        if rownum < 1:
-            break
-        # check if the page has the stock data    
-        if content[1].get_text().find('暂无数据') > -1:
-            stocks.append(['暂无数据', '暂无数据', cat])
-            break
-
-        for stock in content[1:rownum]: #  the first list element is "\n", start from the second one
+    ## FETCH the data from each subclass, loop each page          
+    for i in range(1, pagenum+1):
+        soup = url_request(suburl_p%i)
+        content = soup.find('tbody', attrs={"class": "tbody_right"}).contents
+        for stock in content[1:len(content)-1]: #  the first list element is "\n", start from the second one
             stocks_l = stock.get_text(',').split(',')[0:2] ##stock_cd and stock_nm
             stocks_l.append(cat)
             stocks.append(stocks_l) #create 2-d list, finnally insert into db
-        pgnm += 1
     # print(stocks)
     load_db("Stocks_Details", stocks)
 
 ## fetch subcategory
-def fetch_stock_inds(url): 
-    subNav = url_findresult(url, 'div', {"class": "subNav"})
+def fetch_stock_inds(url):  
+    subNav = url_request(url).find('div', attrs={"class": "subNav"})
     subNav_list = subNav.find_all('a')
     category = []
     for l in subNav_list:
-        link = l['href']
-        cat = cat_find(link)
-        ltext = l.string.strip("· \t\n\r") #remove any space and . at the head/tail
-		# if cat is in the list, find the stock info 
-        if cat:        
-            category.append([cat['code'],ltext,link,cat['cat']])
-            stock_details(link,cat['code'])
+        lurl = 'http://quote.stockstar.com' + l['href']
+        # find current sub category information
+        cat = cat_find(lurl)
+
+        # if cat is in the list, find the stock info 
+        if cat:
+            ltext = l.string.strip("· \t\n\r") #category name:remove any space and . at the head/tail
+            category.append([cat['code'],ltext,lurl,cat['cat']])
+            #find page number info 
+            page = url_request(lurl).find('input', attrs={"id": "ClientPageControl1_hdnTotalCount"})
+
+            # check if current sub category has the stock data: if yes, find the stock details
+            if page.has_attr('value'):
+                pagenum = int(page['value'])//30 + (29+int(page['value'])%30)//30
+                # fetch stock details
+                stock_details(lurl,pagenum,cat['code'])
     # print(category)
     load_db('Sub_Category', category)
 
@@ -114,17 +108,24 @@ def fetch_stock_inds(url):
 # # url = 'http://quote.stockstar.com/stock/ranklist_a_1_0_1.html'
 # # [a] the stock type ;1-0-1 : [1]order by the first column(stock number); [0]: 0-->ascending 1-->desceding;[1]:page number
 
-
 if __name__=='__main__':
+
     ## initialize the global sub category number, to obtain the sub_cat_code
     NUM = {'M':0, 'I':0, 'G':0}
     l = ['/small', '/gem', 'ranklist_', 'industry_', 'blockperformance_3_']
 
     ## connect db, all use the same db
-    dbconn = pymysql.connect(host='localhost', user='root', passwd='root123', db='Feed_Crawler', charset='utf8')
+    try:
+        dbconn = pymysql.connect(host='localhost', user='root', passwd='root123', db='Feed_Crawler', charset='utf8')
+        print('DB connect sucessfully...')
+    except pymysql.Error as e:
+        print("dbconn failed, please check if the server starts and the connect info !!!") 
+    
     cursor = dbconn.cursor() 
 
     ## fetch main page
     url = 'http://quote.stockstar.com/stock/industry.shtml'
     fetch_stock_inds(url)
-    # stock_details("/stock/industry_o.shtml","I")
+
+    cursor.close()
+    dbconn.commit() 
--- stock_info_recnt.py
+++ stock_info_recnt.py
@@ -16,10 +16,12 @@
 import urllib.request
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
-import datetime
+import datetime,time 
 import pymysql
 
-
+# multiprocessing may cause zombie process
+# bug: https://mail.python.org/pipermail/python-bugs-list/2015-December/287698.html
+# http://stackoverflow.com/questions/30506489/python-multiprocessing-leading-to-many-zombie-processes
 
 
 # def writerow_csv(row_line):
@@ -41,6 +43,7 @@ class stock_crawler(object): ##stockstar web
         else:
             self.__sql = "insert into {0} values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)".format(self.__table)
         #when implementing constructor, intialize the variate/parameter and excute the stock fetch
+        print("stock %s is begining at %s"%(self.__table,datetime.datetime.now().strftime('%H:%M:%S')))
         self.excute()
    
     def load_data(self, values):
@@ -51,8 +54,8 @@ class stock_crawler(object): ##stockstar web
 
     @classmethod
     def fetch_stock(self, url): 
-        headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
-        request=urllib.request.Request(url=url,headers=headers)
+        headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
+        request = urllib.request.Request(url=url,headers=headers)
         response = urllib.request.urlopen(request)
         result = response.read().decode('gbk')
         soup = BeautifulSoup(result, "html.parser")
@@ -77,7 +80,7 @@ class stock_crawler(object): ##stockstar web
         for i in range(1,self.__pagenm+1):
             #callback will get stuck, but can call another function by using the return-result
             p.apply_async(self.fetch_stock, args=(self.__url%i,), callback=self.load_data) 
-
+	
         p.close()
         p.join()
 
@@ -105,8 +108,8 @@ def dbconn(db):
 
 if __name__=='__main__':
 
+    start = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     db = dbconn('test')
-
     lstock = [ {'type':'small', 'pgnm':29, 'cursor':db['cursor']}, 
         {'type':'gem', 'pgnm':21, 'cursor':db['cursor']},
         {'type':'a', 'pgnm':107, 'cursor':db['cursor']},
@@ -120,12 +123,15 @@ if __name__=='__main__':
         # print(lstock[i])
         # stock_crawler(lstock[i])
         p.apply_async( stock_crawler(lstock[i]) ) 
-        # t = stock_crawler({'cursor':db['cursor'], 'type':s['type'], 'pagenm':s['pgnm']})    
+		
+         # t = stock_crawler({'cursor':db['cursor'], 'type':s['type'], 'pagenm':s['pgnm']})    
     p.close()
     p.join()
-
     db['cursor'].close()
     db['conn'].commit() 
+
+    end = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print("################# stock fetch end at %s ...,start at %s !!######################\n" %(end,start) )
 
 
 
